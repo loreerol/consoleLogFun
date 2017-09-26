f1df544d802faa57459bbd8bdc714002918e70f9
Lore Hill <Lore@firmex.host>
2017-09-26 18:38:55 -0400

    solved fizzbuzz

--- loopTriangle.js
+++ loopTriangle.js
@@ -5,9 +5,19 @@ document.getElementById("triangle").onclick = function () {
     for (var numb = 0; numb <7; numb = numb+1){
     if (hash.length = numb) hash = (hash + "#");
         console.log(hash);
-    }
+    };
  };
 
-document.getElementById("fizzbuzz").onclick = function () { console.log('boo!'); };
+document.getElementById("fizzbuzz").onclick = function () { 
+    var fizz = "fizz";
+    var buzz = "buzz";
+    var fizzbuzz = "fizzbuzz"
+    for (var numb = 1; numb <101; numb = numb+1){
+        if ((numb % 3 )== 0 & (numb % 5)==0) console.log(fizzbuzz);
+        else if ((numb % 3) == 0) console.log(fizz); 
+        else if ((numb % 5) == 0) console.log(buzz);
+        else console.log(numb);
+    };
+};
 
 document.getElementById("checkerboard").onclick = function () { console.log('boo!'); };
