01eb1bb87a9fac0586526ef1f8868fc7f832b7b8
Lore Hill <Lore@firmex.host>
2017-09-26 13:19:52 -0400

    linked buttons to js functions

--- loopTriangle.js
+++ loopTriangle.js
@@ -1,1 +1,7 @@
-document.getElementById("boo").onclick = console.log(boo);
+document.getElementById("boo").onclick = function () { console.log('boo!'); };
+
+document.getElementById("triangle").onclick = function () { console.log('boo!'); };
+
+document.getElementById("fizzbuzz").onclick = function () { console.log('boo!'); };
+
+document.getElementById("checkerboard").onclick = function () { console.log('boo!'); };
