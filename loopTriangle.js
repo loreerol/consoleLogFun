c161055628fc2ce9676331994db612d0c0082489
Lore Hill <Lore@firmex.host>
2017-09-26 17:19:05 -0400

    Solved triangle puzzle

--- loopTriangle.js
+++ loopTriangle.js
@@ -1,6 +1,12 @@
 document.getElementById("boo").onclick = function () { console.log('boo!'); };
 
-document.getElementById("triangle").onclick = function () { console.log('boo!'); };
+document.getElementById("triangle").onclick = function () { 
+    var hash = "#";
+    for (var numb = 0; numb <7; numb = numb+1){
+    if (hash.length = numb) hash = (hash + "#");
+        console.log(hash);
+    }
+ };
 
 document.getElementById("fizzbuzz").onclick = function () { console.log('boo!'); };
 
