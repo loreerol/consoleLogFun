43d2ca03966a1a2ac5d6c019056cc72ee5080727
Lore Hill <Lore@firmex.host>
2017-09-26 18:53:45 -0400

    improved fizzbuzz answer

--- loopTriangle.js
+++ loopTriangle.js
@@ -11,9 +11,8 @@ document.getElementById("triangle").onclick = function () {
 document.getElementById("fizzbuzz").onclick = function () { 
     var fizz = "fizz";
     var buzz = "buzz";
-    var fizzbuzz = "fizzbuzz"
     for (var numb = 1; numb <101; numb = numb+1){
-        if ((numb % 3 )== 0 & (numb % 5)==0) console.log(fizzbuzz);
+        if ((numb % 3 )== 0 & (numb % 5)==0) console.log(fizz + buzz);
         else if ((numb % 3) == 0) console.log(fizz); 
         else if ((numb % 5) == 0) console.log(buzz);
         else console.log(numb);
